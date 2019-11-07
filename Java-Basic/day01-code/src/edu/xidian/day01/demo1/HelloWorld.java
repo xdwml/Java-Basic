721dc4b1d1a20aa54b1368bc7a38ad3efbd1489f
wml <18270856781@163.com>
2019-11-07 22:46:15 +0800

    Signed-off-by: wml <18270856781@163.com>

---
+++ Java-Basic/day01-code/src/edu/xidian/day01/demo1/HelloWorld.java
@@ -0,0 +1,7 @@
+package edu.xidian.day01.demo1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
---
+++ Java-Basic/day04-code/src/edu/xidian/day04/demo01/HelloWorld.java
@@ -0,0 +1,14 @@
+package edu.xidian.day04.demo01;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,world!!!!");
+        Scanner sc=null;
+
+
+
+    }
+}
