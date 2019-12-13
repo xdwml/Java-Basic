32c75caa212f669a448b5f0810fbed01cc300447
wml <18270856781@163.com>
2019-12-13 19:55:05 +0800

    Signed-off-by: wml <18270856781@163.com>

--- Java-Test/niuke/src/Tree/Problem24.java
+++ Java-Test/niuke/src/Tree/Problem24.java
@@ -1,7 +1,9 @@
 package Tree;
-
+/**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+ * 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+ */
 import java.util.Arrays;
-
 public class Problem24 {
     public static boolean VerifySquenceOfBST(int [] sequence) {
         if(sequence==null||sequence.length<=0) return false;//鲁棒性
@@ -14,29 +16,22 @@ public class Problem24 {
                 break;
             }
         }
-        //在二叉搜索树中右子树的节点大于根节点
-        for(int j=cut;j<sequence.length-1;j++){
-            if (sequence[j]<root){
-                break;
+        if(cut==0)
+            VerifySquenceOfBST(Arrays.copyOfRange(sequence, 0 ,sequence.length-1));
+        else {
+            for(int i=cut;i<sequence.length-1;i++)
+            {
+                if(sequence[i]<=root)
+                    return false;
             }
+            VerifySquenceOfBST(Arrays.copyOfRange(sequence, 0 ,cut));
+            VerifySquenceOfBST(Arrays.copyOfRange(sequence, cut,sequence.length-1));
         }
-        //判断左子树是不是二叉搜索树
-        boolean left=false;
-        boolean right=false;
-
-        if(cut>0){
-            left=VerifySquenceOfBST(Arrays.copyOfRange(sequence,0,cut));
-        }
-        //判断左子树是不是二叉搜索树
-        if(cut<sequence.length-1){
-            right=VerifySquenceOfBST(Arrays.copyOfRange(sequence,cut,sequence.length-1));
-        }
-        return left &&right;
+        return true;
     }
-
     public static void main(String[] args) {
-        int [] arr={4,8,6,12,16,14,10};//true
-        //int [] arr={7,4,5,6};//false
+        //int [] arr={4,8,6,12,16,14,10};//true
+        int [] arr={7,4,5,6};//false
         //int [] arr={5,7,6,9,11,10,8};//true
         System.out.println(VerifySquenceOfBST(arr));
     }
